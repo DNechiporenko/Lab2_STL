a3b02b4a4f90b4da2c63852b4a4f1e324c8eb719
DNechiporenko <114211354+DNechiporenko@users.noreply.github.com>
2024-03-19 22:52:20 +0300

    Add files via upload

---
+++ Lab2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    /*Задание А - Разные (номер 1418)*/
+    int N;
+    cin >> N; //Количество чисел
+
+    set <int> set1; 
+    int num;
+
+    for (int i = 0; i < N; i++) {
+        cin >> num; //Считываем число
+        set1.insert(num); //Записываем в set
+    }
+    cout << set1.size();
+
+    return 0;
+}
