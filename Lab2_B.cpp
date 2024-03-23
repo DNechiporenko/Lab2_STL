e43bcca100db1f11ea37d8f5f537402aaceca604
DNechiporenko <114211354+DNechiporenko@users.noreply.github.com>
2024-03-23 17:42:45 +0300

    Add files via upload

---
+++ Lab2_B.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+
+int main() {
+    std::ifstream inFile("input.txt");
+    std::map<int, std::vector<std::string>> students;
+
+    int classNum;
+    std::string lastName;
+
+    while (inFile >> classNum >> lastName) {
+        students[classNum].push_back(lastName);
+    }
+
+    for (int classNum = 9; classNum <= 11; ++classNum) {
+        for (const auto& lastName : students[classNum]) {
+            std::cout << classNum << " " << lastName << std::endl;
+        }
+    }
+
+    return 0;
+}
