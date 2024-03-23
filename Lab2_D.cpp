c6df0c2745404985b7d5cb2c2d29f828393a3b78
DNechiporenko <114211354+DNechiporenko@users.noreply.github.com>
2024-03-23 17:48:57 +0300

    Add files via upload

---
+++ Lab2_D.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <string>
+
+int main() {
+    std::unordered_set<std::string> set;
+    char operation;
+    std::string word;
+
+    while (true) {
+        std::cin >> operation;
+        if (operation == '#') {
+            break;
+        }
+        std::cin >> word;
+
+        switch (operation) {
+        case '+':
+            set.insert(word);
+            break;
+        case '-':
+            set.erase(word);
+            break;
+        case '?':
+            if (set.find(word) != set.end()) {
+                std::cout << "YES" << std::endl;
+            }
+            else {
+                std::cout << "NO" << std::endl;
+            }
+            break;
+        }
+    }
+
+    return 0;
+}
